f37fbd4a9268167df974a2d2a56d3f919d49a6f1
Daniel Hands <danielhands008@hotmail.co.uk>
2018-07-27 14:48:38 +0100

    Removed Interceptor, now works with PSO

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -18,7 +18,6 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Threading;
-using Interceptor;
 
 namespace osd_buttons
 {
@@ -32,25 +31,180 @@ namespace osd_buttons
             InitializeComponent();
         }
 
-        Input input = new Input();
-
         private KeyboardHook _hook;
-        //InputSimulator sim = new InputSimulator();
+
+        //public const int KEYEVENTF_EXTENDEDKEY = 0x0001; //Key down flag
+        //public const int KEYEVENTF_KEYUP = 0x0002; //Key up flag
+
+        /// <summary>
+        /// http://msdn.microsoft.com/en-us/library/windows/desktop/ms646270(v=vs.85).aspx
+        /// </summary>
+        [StructLayout(LayoutKind.Sequential)]
+        internal struct INPUT
+        {
+            public uint Type;
+            public MOUSEKEYBDHARDWAREINPUT Data;
+        }
+        internal const uint INPUT_MOUSE = 0, INPUT_KEYBOARD = 1, INPUT_HARDWARE = 2;
+        /// <summary>
+        /// http://social.msdn.microsoft.com/Forums/en/csharplanguage/thread/f0e82d6e-4999-4d22-b3d3-32b25f61fb2a
+        /// </summary>
+        [StructLayout(LayoutKind.Explicit)]
+        internal struct MOUSEKEYBDHARDWAREINPUT
+        {
+            [FieldOffset(0)]
+            public HARDWAREINPUT Hardware;
+            [FieldOffset(0)]
+            public KEYBDINPUT Keyboard;
+            [FieldOffset(0)]
+            public MOUSEINPUT Mouse;
+        }
+
+        /// <summary>
+        /// http://msdn.microsoft.com/en-us/library/windows/desktop/ms646310(v=vs.85).aspx
+        /// </summary>
+        [StructLayout(LayoutKind.Sequential)]
+        internal struct HARDWAREINPUT
+        {
+            public uint Msg;
+            public ushort ParamL;
+            public ushort ParamH;
+        }
+
+        /// <summary>
+        /// http://msdn.microsoft.com/en-us/library/windows/desktop/ms646310(v=vs.85).aspx
+        /// </summary>
+        [StructLayout(LayoutKind.Sequential)]
+        internal struct KEYBDINPUT
+        {
+            public ushort Vk;
+            public ushort Scan;
+            public uint Flags;
+            public uint Time;
+            public IntPtr ExtraInfo;
+        }
+
+        /// <summary>
+        /// http://social.msdn.microsoft.com/forums/en-US/netfxbcl/thread/2abc6be8-c593-4686-93d2-89785232dacd
+        /// </summary>
+        [StructLayout(LayoutKind.Sequential)]
+        internal struct MOUSEINPUT
+        {
+            public int X;
+            public int Y;
+            public uint MouseData;
+            public uint Flags;
+            public uint Time;
+            public IntPtr ExtraInfo;
+        }
+
+        internal const uint KEYEVENTF_EXTENDEDKEY = 1, KEYEVENTF_KEYUP = 2, MOUSEEVENTF_WHEEL = 0x0800,
+            MOUSEEVENTF_HWHEEL = 0x1000, MOUSEEVENTF_MIDDLEWDOWN = 0x0020, MOUSEEVENTF_MIDDLEWUP = 0x0040,
+            KEYEVENTF_SCANCODE = 0x0008, MAPVK_VK_TO_VSC = 0, KEYEVENTF_UNICODE = 0x0004;
+
+        internal const uint VK_PAUSE = 0x13, VK_LEFT = 0x25, VK_UP = 0x26, VK_RIGHT = 0x27, VK_DOWN = 0x28,
+    VK_PRIOR = 0x21, VK_NEXT = 0x22, VK_END = 0x23, VK_HOME = 0x24, VK_INSERT = 0x2D, VK_DELETE = 0x2E, VK_APPS = 0x5D,
+    VK_DIVIDE = 0x6F, VK_NUMLOCK = 0x90, VK_RCONTROL = 0xA3, VK_RMENU = 0xA5, VK_BROWSER_HOME = 0xAC,
+    VK_VOLUME_MUTE = 0xAD, VK_VOLUME_DOWN = 0xAE, VK_VOLUME_UP = 0xAF,
+    VK_MEDIA_NEXT_TRACK = 0xB0, VK_MEDIA_PREV_TRACK = 0xB1, VK_MEDIA_STOP = 0xB2, VK_MEDIA_PLAY_PAUSE = 0xB3,
+    VK_LAUNCH_MAIL = 0xB4, VK_LAUNCH_MEDIA_SELECT = 0xB5, VK_LAUNCH_APP1 = 0xB6, VK_LAUNCH_APP2 = 0xB7, EXTENDED_FLAG = 0x100;
+
+        private static INPUT[] sendInputs = new INPUT[2];
+
         [DllImport("user32.dll", SetLastError = true)]
-        static extern void keybd_event(byte bVk, byte bScan, int dwFlags, int dwExtraInfo);
-        public const int KEYEVENTF_EXTENDEDKEY = 0x0001; //Key down flag
-        public const int KEYEVENTF_KEYUP = 0x0002; //Key up flag
-        public const int VK_0 = 0x30;
-        public const int VK_1 = 0x31;
-        public const int VK_2 = 0x32;
-        public const int VK_3 = 0x33;
-        public const int VK_4 = 0x34;
-        public const int VK_5 = 0x35;
-        public const int VK_6 = 0x36;
-        public const int VK_7 = 0x37;
-        public const int VK_8 = 0x38;
-        public const int VK_9 = 0x39;
-        
+        private static extern uint SendInput(uint numberOfInputs, INPUT[] inputs, int sizeOfInputs);
+        [DllImport("user32.dll", CharSet = CharSet.Auto)]
+        private static extern ushort MapVirtualKey(uint uCode, uint uMapType);
+
+        public static void performKeyPress(ushort key)
+        {
+            lock ("keypress")
+            {
+                ushort scancode = scancodeFromVK(key);
+                bool extended = (scancode & 0x100) != 0;
+                uint curflags = extended ? KEYEVENTF_EXTENDEDKEY : 0;
+
+                sendInputs[0].Type = INPUT_KEYBOARD;
+                sendInputs[0].Data.Keyboard.ExtraInfo = IntPtr.Zero;
+                sendInputs[0].Data.Keyboard.Flags = curflags;
+                sendInputs[0].Data.Keyboard.Scan = scancode;
+                //sendInputs[0].Data.Keyboard.Flags = 1;
+                //sendInputs[0].Data.Keyboard.Scan = 0;
+                sendInputs[0].Data.Keyboard.Time = 0;
+                sendInputs[0].Data.Keyboard.Vk = key;
+                uint result = SendInput(1, sendInputs, Marshal.SizeOf(sendInputs[0]));
+            }
+        }
+
+        public static void performKeyRelease(ushort key)
+        {
+            lock ("keypress")
+            {
+                ushort scancode = scancodeFromVK(key);
+                bool extended = (scancode & 0x100) != 0;
+                uint curflags = extended ? KEYEVENTF_EXTENDEDKEY : 0;
+
+                sendInputs[0].Type = INPUT_KEYBOARD;
+                sendInputs[0].Data.Keyboard.ExtraInfo = IntPtr.Zero;
+                sendInputs[0].Data.Keyboard.Flags = curflags | KEYEVENTF_KEYUP;
+                sendInputs[0].Data.Keyboard.Scan = scancode;
+                //sendInputs[0].Data.Keyboard.Flags = KEYEVENTF_EXTENDEDKEY | KEYEVENTF_KEYUP;
+                //sendInputs[0].Data.Keyboard.Scan = 0;
+                sendInputs[0].Data.Keyboard.Time = 0;
+                sendInputs[0].Data.Keyboard.Vk = key;
+                uint result = SendInput(1, sendInputs, Marshal.SizeOf(sendInputs[0]));
+            }
+        }
+
+        private static ushort scancodeFromVK(uint vkey)
+        {
+            ushort scancode = 0;
+            if (vkey == VK_PAUSE)
+            {
+                // MapVirtualKey does not work with VK_PAUSE
+                scancode = 0x45;
+            }
+            else
+            {
+                scancode = MapVirtualKey(vkey, MAPVK_VK_TO_VSC);
+            }
+
+            switch (vkey)
+            {
+                case VK_LEFT:
+                case VK_UP:
+                case VK_RIGHT:
+                case VK_DOWN:
+                case VK_PRIOR:
+                case VK_NEXT:
+                case VK_END:
+                case VK_HOME:
+                case VK_INSERT:
+                case VK_DELETE:
+                case VK_DIVIDE:
+                case VK_NUMLOCK:
+                case VK_RCONTROL:
+                case VK_RMENU:
+                case VK_VOLUME_MUTE:
+                case VK_VOLUME_DOWN:
+                case VK_VOLUME_UP:
+                case VK_MEDIA_NEXT_TRACK:
+                case VK_MEDIA_PREV_TRACK:
+                case VK_LAUNCH_MEDIA_SELECT:
+                case VK_BROWSER_HOME:
+                case VK_LAUNCH_MAIL:
+                case VK_LAUNCH_APP1:
+                case VK_LAUNCH_APP2:
+                case VK_APPS:
+                    {
+                        scancode |= (ushort)EXTENDED_FLAG; // set extended bit
+                        break;
+                    }
+            }
+
+            return scancode;
+        }
+
 
         int place = 1;
 
@@ -60,8 +214,6 @@ namespace osd_buttons
             _hook = new KeyboardHook();
             _hook.KeyDown += new KeyboardHook.HookEventHandler(OnHookKeyDown);
             setvis();
-            input.KeyboardFilterMode = KeyboardFilterMode.All;
-            input.Load();
 
         }
         // keyboard hook handler
@@ -82,34 +234,54 @@ namespace osd_buttons
                 switch(place)
                 {
                     case 1:
-                        input.SendKeys(Keys.One);
+                        performKeyPress(49);
+                        Thread.Sleep(20);
+                        performKeyRelease(49);
                         break;
                     case 2:
-                        input.SendKeys(Keys.Two);
+                        performKeyPress(50);
+                        Thread.Sleep(20);
+                        performKeyRelease(50);
                         break;
                     case 3:
-                        input.SendKeys(Keys.Three);
+                        performKeyPress(51);
+                        Thread.Sleep(20);
+                        performKeyRelease(51);
                         break;
                     case 4:
-                        input.SendKeys(Keys.Four);
+                        performKeyPress(52);
+                        Thread.Sleep(20);
+                        performKeyRelease(52);
                         break;
                     case 5:
-                        input.SendKeys(Keys.Five);
+                        performKeyPress(53);
+                        Thread.Sleep(20);
+                        performKeyRelease(53);
                         break;
                     case 6:
-                        input.SendKeys(Keys.Six);
+                        performKeyPress(54);
+                        Thread.Sleep(20);
+                        performKeyRelease(54);
                         break;
                     case 7:
-                        input.SendKeys(Keys.Seven);
+                        performKeyPress(55);
+                        Thread.Sleep(20);
+                        performKeyRelease(55);
                         break;
                     case 8:
-                        input.SendKeys(Keys.Eight);
+                        performKeyPress(56);
+                        Thread.Sleep(20);
+                        performKeyRelease(56);
                         break;
                     case 9:
-                        input.SendKeys(Keys.Nine);
+                        performKeyPress(57);
+                        Thread.Sleep(20);
+                        performKeyRelease(57);
                         break;
                     case 10:
-                        input.SendKeys(Keys.Zero);
+                        performKeyPress(48);
+                        Thread.Sleep(20);
+                        performKeyRelease(48);
                         break;
                     default:
                         break;
