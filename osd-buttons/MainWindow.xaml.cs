9e9424c77973283555028d7d7af5c8ce857e64e7
Daniel Hands <danielhands008@hotmail.co.uk>
2018-08-01 16:43:11 +0100

    Basic onscreen controls have been added for PSO

--- osd-buttons/MainWindow.xaml.cs
+++ osd-buttons/MainWindow.xaml.cs
@@ -33,6 +33,11 @@ namespace osd_buttons
         
         int place = 1;
 
+        Point StickStartPoint = new Point(0, 0);
+        Point StickCurrentPoint = new Point(0, 0);
+        Point StickDeltaPoint = new Point(0, 0);
+        bool StickDown = false, left = false, right = false, up = false, down = false;
+
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             // install system-wide keyboard hook
@@ -108,6 +113,150 @@ namespace osd_buttons
             if (place == 10) rectangle_0.Visibility = Visibility.Visible; else rectangle_0.Visibility = Visibility.Hidden;
         }
 
+
+        protected override void OnManipulationStarted(ManipulationStartedEventArgs e)
+        { 
+            StickStartPoint = e.ManipulationOrigin;
+            StickCurrentPoint = e.ManipulationOrigin;
+            StickDown = true;
+
+        }
+
+        protected override void OnManipulationDelta(ManipulationDeltaEventArgs e)
+        {
+            StickCurrentPoint = e.ManipulationOrigin;
+            StickDeltaPoint.X = StickCurrentPoint.X - StickStartPoint.X;
+            StickDeltaPoint.Y = StickCurrentPoint.Y - StickStartPoint.Y;
+            if (StickDeltaPoint.X > 50)
+            {
+                right = true;
+                KeyboardOutput.performKeyDown(68);
+            }
+            else
+            {
+                right = false;
+                KeyboardOutput.performKeyRelease(68);
+            }
+            if (StickDeltaPoint.X < -50)
+            {
+                left = true;
+                KeyboardOutput.performKeyDown(65);
+            }
+            else
+            {
+                left = false;
+                KeyboardOutput.performKeyRelease(65);
+            }
+            if (StickDeltaPoint.Y > 50)
+            {
+                down = true;
+                KeyboardOutput.performKeyDown(83);
+            }
+            else
+            {
+                down = false;
+                KeyboardOutput.performKeyRelease(83);
+            }
+            if (StickDeltaPoint.Y < -50)
+            {
+                up = true;
+                KeyboardOutput.performKeyDown(87);
+            }
+            else
+            {
+                up = false;
+                KeyboardOutput.performKeyRelease(87);
+            }
+        }
+
+        protected override void OnManipulationCompleted(ManipulationCompletedEventArgs e)
+        {
+            if (right)
+            {
+                KeyboardOutput.performKeyRelease(68);
+            }
+            if (left)
+            {
+                KeyboardOutput.performKeyRelease(65);
+            }
+            if (up)
+            {
+                KeyboardOutput.performKeyRelease(87);
+            }
+            if (down) {
+                KeyboardOutput.performKeyRelease(83);
+            }
+            StickDown = up = down = left = right = false;
+            StickDeltaPoint.X = 0;
+            StickDeltaPoint.Y = 0;
+        }
+
+
+        private void up_TouchDown(object sender, EventArgs e)
+        {
+            KeyboardOutput.performKeyDown(38);
+        }
+        private void up_TouchUp(object sender, EventArgs e)
+        {
+            KeyboardOutput.performKeyRelease(38);
+        }
+        private void down_TouchDown(object sender, EventArgs e)
+        {
+            KeyboardOutput.performKeyDown(40);
+        }
+        private void down_TouchUp(object sender, EventArgs e)
+        {
+            KeyboardOutput.performKeyRelease(40);
+        }
+        private void left_TouchDown(object sender, EventArgs e)
+        {
+            KeyboardOutput.performKeyDown(37);
+        }
+        private void left_TouchUp(object sender, EventArgs e)
+        {
+            KeyboardOutput.performKeyRelease(37);
+        }
+        private void right_TouchDown(object sender, EventArgs e)
+        {
+            KeyboardOutput.performKeyDown(39);
+        }
+        private void right_TouchUp(object sender, EventArgs e)
+        {
+            KeyboardOutput.performKeyRelease(39);
+        }
+
+        private void F12_TouchDown(object sender, EventArgs e)
+        {
+            KeyboardOutput.performKeyDown(123);
+        }
+        private void F12_TouchUp(object sender, EventArgs e)
+        {
+            KeyboardOutput.performKeyRelease(123);
+        }
+        private void enter_TouchDown(object sender, EventArgs e)
+        {
+            KeyboardOutput.performKeyDown(13);
+        }
+        private void enter_TouchUp(object sender, EventArgs e)
+        {
+            KeyboardOutput.performKeyRelease(13);
+        }
+        private void end_TouchDown(object sender, EventArgs e)
+        {
+            KeyboardOutput.performKeyDown(35);
+        }
+        private void end_TouchUp(object sender, EventArgs e)
+        {
+            KeyboardOutput.performKeyRelease(35);
+        }
+        private void ctrl_TouchDown(object sender, EventArgs e)
+        {
+            KeyboardOutput.performKeyDown(162);
+        }
+        private void ctrl_TouchUp(object sender, EventArgs e)
+        {
+            KeyboardOutput.performKeyRelease(162);
+        }
 
 
         private void buttonKeyboard_Click(object sender, RoutedEventArgs e)
